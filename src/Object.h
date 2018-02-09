7609bfe55bc1ed33a71aa1d2d5c04bdb651d0047
magestik <bastien@magestik.fr>
2018-02-09 22:37:18 +0100

    Removed useless include

--- src/Object.h
+++ src/Object.h
@@ -3,14 +3,11 @@
 #include <Vector.h>
 #include <Matrix.h>
 
-#include <vector> // std::vector
-
 class Mesh;
 
 struct Object
 {
 	Object(Mesh * m) : transformation(1.0f), mesh(m) { /* ... */ }
-	//const std::vector<SubMesh*> & getDrawCommands() const { return(mesh->m_aSubMeshes); }
 	mat4x4 transformation;
 	Mesh * mesh;
 };
